eb3f6d6658519c78f0d16a5ae6de91d763cf8ec5
sigamugacaca <ignasbernotas@outlook.com>
2018-02-26 22:06:20 +0200

    Hello

---
+++ practice/11.2/index.php
@@ -0,0 +1,19 @@
+<?php
+class Ivykis {
+    public $pavadinimas;
+    public $data;
+
+    public function __construct($pavadinimas, \DateTime $dateTime)
+    {
+        $this->pavdinimas = $dateTime;
+        $this->data = $dataTime;
+    }
+class Istorija {
+    public $ivykis;
+    public $
+    }
+}
+}
+$ivykis = new Ivykis(pavadinimas:'Php Kaunas', new \DateTime (time '2018-02-05'));
+
+?>
